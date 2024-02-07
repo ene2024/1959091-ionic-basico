7ae8c68bf48af0a9789f73e48e3479ecdf41131b
diegoadrianm <diegomorduar@gmail.com>
2024-02-06 21:42:37 -0600

    Practica 1

---
+++ script.js
@@ -0,0 +1,18 @@
+var miVariable = 5;
+
+var miTxt = document.getElementById("txt");
+var texto = "";
+
+function miFuncion(){
+    texto = miTxt.value;
+    alert(texto);
+  
+}
+
+function limpiar(){
+    miTxt.value = "";
+}
+
+function cambiarPagina(){
+    
+}
